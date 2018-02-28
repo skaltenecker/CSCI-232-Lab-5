8283e161ca8796b0283ed7cf571ef637524ad1db
Scott Kaltenecker <skalt@cableone.net>
2018-02-27 17:40:19 -0700

    First Git commit of all time

---
+++ src/lab5/HelloWorld.java
@@ -0,0 +1,13 @@
+package lab5;
+
+/**
+ *Author: Scott Kaltenecker
+ * 
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
